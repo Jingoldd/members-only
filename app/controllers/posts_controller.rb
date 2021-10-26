69f4b629d6c72317726974517a519833a43a0f3b
Javier Ingold <ingold.javer@gmail.com>
2021-10-26 20:46:13 -0300

    post model and methods created

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post[:user_id] = current_user.id
+
+    if @post.save
+      redirect_to posts_path, notice: 'Created successfully!'
+    else
+      render :new 
+    end
+  end 
+
+  private 
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end 
+end
