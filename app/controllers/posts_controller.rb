489267413f9af2e0ec5829a4b4e4a5f37879d6ab
Javier Ingold <ingold.javer@gmail.com>
2021-11-01 16:46:12 -0300

    edit and destroy methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, except: [:index]
+  before_action :find_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -20,7 +21,27 @@ class PostsController < ApplicationController
     end
   end 
 
+  def edit
+  end 
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: 'Post updated!'
+    else
+      render :edit  
+    end
+  end 
+
+  def destroy
+    @post.destroy
+    redirect_to root_path, notice: 'Post deleted successfully'
+  end 
+
   private 
+
+  def find_post
+    @post = Post.find(params[:id])
+  end 
 
   def post_params
     params.require(:post).permit(:title, :body)
